9e6da5972d2090d66818f0da08399c72a61fca31
Esteban Magoja <estebanmagoja@gmail.com>
2025-09-18 15:43:31 -0300

    redireccion luego de registro

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,4 +4,12 @@ namespace App\Http\Controllers\Auth;
 
 use Wave\Http\Controllers\Auth\RegisterController as AuthRegisterController;
 
-class RegisterController extends AuthRegisterController {}
+class RegisterController extends AuthRegisterController
+{
+    /**
+     * Where to redirect users after registration.
+     *
+     * @var string
+     */
+    public $redirectTo = '/dashboard';
+}
