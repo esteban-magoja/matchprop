c3d0a19d36853c3103b7a51d222d2accdd062047
Esteban Magoja <estebanmagoja@gmail.com>
2025-11-25 15:18:16 -0300

    [i18n] Fix: Use correct Filament v4 Tabs namespace (Schemas)

--- app/Filament/Resources/Pages/PageResource.php
+++ app/Filament/Resources/Pages/PageResource.php
@@ -5,7 +5,7 @@ namespace App\Filament\Resources\Pages;
 use BackedEnum;
 use Filament\Schemas\Schema;
 use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\Tabs;
+use Filament\Schemas\Components\Tabs;
 use Filament\Schemas\Components\Utilities\Set;
 use Filament\Forms\Components\RichEditor;
 use Filament\Forms\Components\Textarea;
